dba417f9dcf63aff256e68fac150dde1910c9622
Boris Bugor <b.bugor@headsandhands.ru>
2023-08-03 19:54:33 +0300

    Initial Commit

---
+++ Multipeer Chat/Multipeer_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multipeer_ChatApp.swift
+//  Multipeer Chat
+//
+//  Created by Boris Bugor on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Multipeer_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
